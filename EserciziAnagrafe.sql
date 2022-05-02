d319db77a11763307d82037e38671144b055a424
marcamm00 <marco.cammarata@hotmail.it>
2022-05-02 15:37:37 +0200

    terza query

--- EserciziAnagrafe.sql
+++ EserciziAnagrafe.sql
@@ -1,7 +1,12 @@
+--1
 select persona.Nome, città.Regione
 from persona join città on persona.CittàNascita = città.Nome
 where persona.Età >= 18
-
+--2
 select g.Nome as genitore, f.Nome as figlio
 from persona g join genia on g.Nome = genia.Genitore join persona f on f.Nome = genia.Figlio
 where g.CittàNascita = f.CittàNascita
+--3
+select distinct p.CittàNascita
+from persona p join genia on p.Nome = genia.Genitore
+where p.Età >= 50
