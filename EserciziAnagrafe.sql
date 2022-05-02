26c5a07b2122dc7debb70c61a401b6d0ff8dbb01
marcamm00 <marco.cammarata@hotmail.it>
2022-05-02 15:20:05 +0200

    seconda query

--- EserciziAnagrafe.sql
+++ EserciziAnagrafe.sql
@@ -1,3 +1,7 @@
 select persona.Nome, città.Regione
 from persona join città on persona.CittàNascita = città.Nome
 where persona.Età >= 18
+
+select g.Nome as genitore, f.Nome as figlio
+from persona g join genia on g.Nome = genia.Genitore join persona f on f.Nome = genia.Figlio
+where g.CittàNascita = f.CittàNascita
