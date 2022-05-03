a68624c67c24a8f54f9210f14375f0740c709783
marcamm00 <marco.cammarata@hotmail.it>
2022-05-03 11:10:13 +0200

    query 2

--- EserciziProgrammatori.sql
+++ EserciziProgrammatori.sql
@@ -4,3 +4,8 @@ from  programmatore p join autore a on p.codice = a.codice join programma
     on a.id = programma.id
 where programma.anno > 2000 and programma.linguaggio = "Java"
 --2
+select p.nome, p.categoria
+from  programmatore p join autore a on p.codice = a.codice join programma 
+    on a.id = programma.id
+where programma.linguaggio != "Python"
+order by p.nome
