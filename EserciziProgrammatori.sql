1144916e384d3a135b25cde4497ab9418930ea1f
marcamm00 <marco.cammarata@hotmail.it>
2022-05-03 12:04:20 +0200

    query 4

--- EserciziProgrammatori.sql
+++ EserciziProgrammatori.sql
@@ -14,3 +14,9 @@ select distinct p.codice, programma.anno
 from  programmatore p join autore a on p.codice = a.codice join programma 
     on a.id = programma.id
 where programma.linguaggio != "Java" and p.categoria = 10
+--4
+select a1.codice as p1, a2.codice as p2
+from autore a1 join programma on a1.id = programma.id 
+    join autore a2 on a2.id = programma.id
+where programma.linguaggio = "Python" and a1.codice != a2.codice and a2.codice < a1.codice
+
