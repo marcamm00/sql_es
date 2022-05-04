4a247dcc008c878c988e0c5fd298dd47eab1e349
marcamm00 <marco.cammarata@hotmail.it>
2022-05-04 11:29:22 +0200

    query 2

--- EserciziScalatori.sql
+++ EserciziScalatori.sql
@@ -1,3 +1,10 @@
 --1
 select distinct nazione.continente, scalata.scalatore as Scalatore
 from scalata join nazione on scalata.nazione = nazione.nome
+--2
+select s.CF, count(nazione.continente) as nContinenti
+from scalatore s join scalata on s.CF = scalata.scalatore
+    join nazione on scalata.nazione = nazione.nome
+where s.annoNascita < 1980
+group by s.CF
+order by s.CF, nazione.continente
