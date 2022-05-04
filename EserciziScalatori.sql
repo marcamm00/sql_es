ce0a4038b4400ed228bc1a05d141a33e09c27c53
marcamm00 <marco.cammarata@hotmail.it>
2022-05-04 11:37:46 +0200

    query 3

--- EserciziScalatori.sql
+++ EserciziScalatori.sql
@@ -8,3 +8,8 @@ from scalatore s join scalata on s.CF = scalata.scalatore
 where s.annoNascita < 1980
 group by s.CF
 order by s.CF, nazione.continente
+--3
+select distinct nazione.nome, nazione.continente
+from nazione join scalata on nazione.nome = scalata.nazione
+    join scalatore on scalata.scalatore = scalatore.CF
+where (scalata.anno-scalatore.annoNascita)<18
